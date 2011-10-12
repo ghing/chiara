0db5b23a60b298a3f6e71f7f86a111660b5a9eef
Geoffrey Hing <geoff@terrorware.com>
2011-10-12 11:41:31 -0400

    Replaced echo statements with ugly string concatenations with cleaner printfs

--- attachment_thumbnails.php
+++ attachment_thumbnails.php
@@ -10,9 +10,13 @@ if ($attachments) {
   foreach ($attachments as $attachment) {
     if ($attachment->ID != get_post_thumbnail_id()) {
       $image_attributes = wp_get_attachment_image_src($attachment->ID, 'original');
-      echo '<a href="' . $image_attributes[0] . '" class="attachment-src-url" title="' . $attachment->post_title . '">' . wp_get_attachment_image($attachment->ID, 'object-thumbnail') . '</a>';
+      printf("<div class=\"attachment-%d\">", $attachment->ID);
+      printf("<a href=\"%s\" class=\"attachment-src-url\" title=\"%s\">%s</a>",
+              $image_attributes[0], $attachment->post_title, 
+              wp_get_attachment_image($attachment->ID, 'object-thumbnail'));
       echo '<div class="attachment-title">' . $attachment->post_title . '</div>';
       echo '<div class="attachment-description">' . $attachment->post_content . '</div>';
+      echo("</div>");
     }
   }
 }
