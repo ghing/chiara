14ed374e34b52aef13a309d6bf54eeca83538616
Geoffrey Hing <geoff@terrorware.com>
2011-10-12 11:53:35 -0400

    Broke display of large image out into separate file.

---
+++ featured_image.php
@@ -0,0 +1,9 @@
+<?php if ( has_post_thumbnail() ): 
+  $original_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');  
+?>
+<div class="entry-main-image attachment-<?php echo get_post_thumbnail_id(); ?>">
+  <a href="<?php echo $original_image_url[0]; ?>" title="<?php chiara_the_post_thumbnail_title(); ?>"><?php the_post_thumbnail('object'); ?></a>
+  <div class="attachment-title"><?php chiara_the_post_thumbnail_title(); ?></div>
+  <div class="attachment-description"><?php chiara_the_post_thumbnail_description(); ?></div>
+</div>
+<?php endif; ?>
--- single-chiara_object.php
+++ single-chiara_object.php
@@ -12,15 +12,7 @@ get_header();
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?> 
 <h2 class="entry-title"><?php the_title(); ?></h2>
-<?php if ( has_post_thumbnail() ): 
-  $original_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');  
-?>
-<div class="entry-main-image attachment-<?php echo get_post_thumbnail_id(); ?>">
-  <a href="<?php echo $original_image_url[0]; ?>" title="<?php chiara_the_post_thumbnail_title(); ?>"><?php the_post_thumbnail('object'); ?></a>
-  <div class="attachment-title"><?php chiara_the_post_thumbnail_title(); ?></div>
-  <div class="attachment-description"><?php chiara_the_post_thumbnail_description(); ?></div>
-</div>
-<?php endif; ?>
+<?php get_template_part( 'featured_image' ); ?>
 <?php get_template_part( 'attachment_thumbnails' ); ?>
 <?php endwhile; endif; ?>
 
