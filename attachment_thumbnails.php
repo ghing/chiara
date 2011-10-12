882659927639edc62466d3b0dba23fdeeb87ec42
Geoffrey Hing <geoff@terrorware.com>
2011-10-12 11:48:45 -0400

    Made thumbnails float and have better spacing.

--- attachment_thumbnails.php
+++ attachment_thumbnails.php
@@ -10,7 +10,7 @@ if ($attachments) {
   foreach ($attachments as $attachment) {
     if ($attachment->ID != get_post_thumbnail_id()) {
       $image_attributes = wp_get_attachment_image_src($attachment->ID, 'original');
-      printf("<div class=\"attachment-%d\">", $attachment->ID);
+      printf("<div class=\"attachment-%d grid_3\">", $attachment->ID);
       printf("<a href=\"%s\" class=\"attachment-src-url\" title=\"%s\">%s</a>",
               $image_attributes[0], $attachment->post_title, 
               wp_get_attachment_image($attachment->ID, 'object-thumbnail'));
