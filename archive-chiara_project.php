30473f1e3ccba83ba81d94936a97bf5efba9d66e
Geoffrey Hing <geoff@terrorware.com>
2011-10-12 22:51:45 -0400

    Order project and object post archives by date.

--- archive-chiara_project.php
+++ archive-chiara_project.php
@@ -17,7 +17,9 @@ get_header();
 ?>
 <h2><?php wp_title(''); ?></h2>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : ?>
+<?php query_posts($query_string . "&order_by=date&order=asc"); ?>
+<?php while (have_posts()) : the_post(); ?>
 <div class="grid_3 small-post-container"> 
 	<h3 class="entry-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
 	<a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail('chiara-thumbnail-220-square'); ?></a>	
