57ff1fd45cd74ba67a4f3a1ec57e19a9e1de3b7f
Geoffrey Hing <geoff@terrorware.com>
2011-10-12 22:29:33 -0400

    Fixed code that I accidently deleted with the last commit.

--- attachments_big.php
+++ attachments_big.php
@@ -11,6 +11,9 @@ if ($attachments) {
     if ($attachment->ID != get_post_thumbnail_id()) {
       $image_attributes = wp_get_attachment_image_src($attachment->ID, 'original');
       printf("<div class=\"attachment-%d attachment-big \">", $attachment->ID);
+      printf("<a href=\"%s\" class=\"attachment-src-url\" title=\"%s\">%s</a>",
+             $image_attributes[0], $attachment->post_title, 
+             wp_get_attachment_image($attachment->ID, 'chiara-700'));
       chiara_attachment_caption($attachment->ID);
       echo("</div>");
     }
