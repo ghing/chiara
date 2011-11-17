c89614378ad7674184e564e43f6f26f32ad1a327
Geoffrey Hing <geoff@terrorware.com>
2011-11-17 02:46:41 -0500

    Added template for static pages.

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The Template for displaying a page. 
+ *
+ * @package WordPress
+ * @subpackage Chiara 
+ * @since Chiara 1.0
+ */
+
+get_header(); 
+?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?> 
+<h2 class="entry-title"><?php the_title(); ?></h2>
+<?php the_content(); ?>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
